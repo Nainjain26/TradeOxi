597ce4227604dd483024c58103ee1a55d74d9e23
Nainjain26 <naina.jain2606@gmail.com>
2025-01-29 18:22:34 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 // import Image from "next/image";
 import Numbers from "./components/Numbers";
 import Accounts from "./components/Accounts";
@@ -8,8 +9,26 @@ import Process from "./components/Process";
 import Testimonals from "./components/Testimonals";
 import Download from "./components/Download";
 import Hero from "./components/Hero";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+      const fetchBlogs = async () => {
+        try {
+          const response = await fetch('https://adminbackend-iypc.onrender.com/uploads');
+          if (!response.ok) {
+            throw new Error('Failed to fetch data');
+          }
+          const blogData = await response.json();
+          console.log(blogData.images)
+          
+        } catch (err) {
+          console.error('Error fetching data:', err);
+        }
+      };
+  
+      fetchBlogs();
+    }, []);
   return (
 <div>
     <Hero/>
