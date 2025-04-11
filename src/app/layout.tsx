0c24a14f2a12508e6de78c7886ba5b71f3cf0e0f
Nainjain26 <naina.jain2606@gmail.com>
2025-04-11 17:27:14 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,39 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "TradeOxi",
-  description: "Tradeoxi empowers stock market enthusiasts with trading strategies, market insights, analytics, and tools for smarter investment decisions globally.It includes features like :- Forex Trading ,Investment Advisor, Forex signals, gold signals ,risk Management , forex trading , investment advisor, Currency Trading , currency trading , trading platform , stock market account , forex exchange, Gold Signals , Risk Management, forex signals , Forex Exchange",
-  //   icons: {
-  //   icon: '/favicon.ico',
-  // },
+  title: "TradeOxi | Forex, Gold & Investment Insights",
+  description:
+    "TradeOxi empowers traders with expert forex trading signals, gold insights, investment strategies, and real-time market analytics. Access powerful tools for smarter global trading decisions.",
+  keywords: [
+    "forex trading",
+    "investment advisor",
+    "forex signals",
+    "gold signals",
+    "risk management",
+    "currency trading",
+    "trading platform",
+    "stock market account",
+    "forex exchange",
+    "global trading insights",
+    "smart investment tools"
+  ],
+  openGraph: {
+    title: "TradeOxi | Forex, Gold & Investment Insights",
+    description:
+      "TradeOxi delivers reliable forex signals, risk management tools, and expert investment strategies for smarter, global stock and forex trading.",
+    url: "https://www.tradeoxi.com",
+    siteName: "TradeOxi",
+    images: [
+      {
+        url: "f3.jpg", 
+        width: 1200,
+        height: 630,
+        alt: "TradeOxi Trading Dashboard",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 
 };
 
